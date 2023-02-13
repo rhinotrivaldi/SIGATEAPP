830e761547809527f16c046aa1070dd7498eb2e1
Rhino Trivaldi <rhino.trivaldi@yahoo.com>
2023-02-14 04:06:37 +0700

    First Commit

--- app/src/main/java/com/rhinotrivaldi/sigate/fragment/HomeFragment.kt
+++ app/src/main/java/com/rhinotrivaldi/sigate/fragment/HomeFragment.kt
@@ -5,18 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager.widget.ViewPager
 import com.rhinotrivaldi.sigate.R
+import com.rhinotrivaldi.sigate.adapter.AdapterSlider
 
 
 class HomeFragment : Fragment() {
 
+    lateinit var vpSlider: ViewPager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
+
+        vpSlider = view.findViewById(R.id.vp_slider)
+
+        val arrSlider = ArrayList<Int>()
+        arrSlider.add(R.drawable.mobil1)
+        arrSlider.add(R.drawable.mobil2)
+        arrSlider.add(R.drawable.mobil3)
+
+        val adapterSlider = AdapterSlider(arrSlider, activity)
+        vpSlider.adapter = adapterSlider
+
+        return view
     }
 
 }
