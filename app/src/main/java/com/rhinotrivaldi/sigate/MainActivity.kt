9d5db144daaa6c2983de53f75a882019066e8d1f
Rhino Trivaldi <rhino.trivaldi@yahoo.com>
2023-02-14 03:21:42 +0700

    First Commit

--- app/src/main/java/com/rhinotrivaldi/sigate/MainActivity.kt
+++ app/src/main/java/com/rhinotrivaldi/sigate/MainActivity.kt
@@ -2,8 +2,12 @@ package com.rhinotrivaldi.sigate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.rhinotrivaldi.sigate.fragment.AccountFragment
 import com.rhinotrivaldi.sigate.fragment.BagFragment
 import com.rhinotrivaldi.sigate.fragment.HomeFragment
@@ -15,15 +19,49 @@ class MainActivity : AppCompatActivity() {
     val fragmentBag: Fragment = BagFragment()
 
     val fm:FragmentManager = supportFragmentManager
-
     var active: Fragment = fragmentHome
+
+    private lateinit var menu: Menu
+    private lateinit var menuItem: MenuItem
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setUpBottomNav()
+
+    }
+
+    private fun setUpBottomNav(){
         fm.beginTransaction().add(R.id.container, fragmentHome).show(fragmentHome).commit()
         fm.beginTransaction().add(R.id.container, fragmentBag).hide(fragmentBag).commit()
         fm.beginTransaction().add(R.id.container, fragmentAccount).hide(fragmentAccount).commit()
 
+        bottomNavigationView = findViewById(R.id.nav_view)
+        menu = bottomNavigationView.menu
+        menuItem = menu.getItem(0)
+        menuItem.isChecked = true
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+
+            when(item.itemId){
+                R.id.navigation_home ->{
+                    callFragment(0, fragmentHome)
+                }
+                R.id.navigation_bag ->{
+                    callFragment(1, fragmentBag)
+                }
+                R.id.navigation_account ->{
+                    callFragment(2, fragmentAccount)
+                }
+            }
+            false
+        }
+    }
+    private fun callFragment(int: Int, fragment: Fragment){
+        menuItem = menu.getItem(int)
+        menuItem.isChecked = true
+        fm.beginTransaction().hide(active).show(fragment).commit()
+        active = fragment
     }
 }
